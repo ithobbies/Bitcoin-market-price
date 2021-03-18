b0471575c6f158ee0b09f8a91226b806d5e07b0f
ithobbies <dannyfeel93@gmail.com>
2021-03-18 22:41:59 +0200

    add timespan and some GUI features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QLineEdit
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
@@ -11,13 +11,20 @@ import requests
 class MainWindow(QDialog):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-
+        self.init_main()
+    
+    # инициализация GUI 
+    def init_main(self):
         # экземпляр фигуры для построения графика
         self.figure = plt.figure()
 
         # Canvas Widget отображающий `figure`
         # он принимает экземпляр `figure` в качестве параметра __init__
         self.canvas = FigureCanvas(self.figure)
+
+        # Поле ввода периода цен
+        self.timespan = QLineEdit()
+        self.timespan.setPlaceholderText('Введите период в годах:')
 
         # кнопка вызова функции построения графика
         self.button = QPushButton('Построить график')
@@ -26,14 +33,18 @@ class MainWindow(QDialog):
         # создаем вертикальный контейнер и помещаем виджеты
         layout = QVBoxLayout()
         layout.addWidget(self.canvas)
+        layout.addWidget(self.timespan)
         layout.addWidget(self.button)
         self.setLayout(layout)
 
-    # функция получения данных через API
-    def get_data_plot(self):        
+    # функция получения данных через API и периода получаемых цен
+    def get_data_plot(self):     
+        # получаем значение в годах из поля ввода timespan 
+        time = self.timespan.text()        
         # запрос и получение данных через API blockchain.com
         url = 'https://api.blockchain.info/charts/market-price'
         params = {'timespan': '10year', 'rollingAverage': '8hours', 'format': 'json'}
+        params['timespan'] = time + 'year'
         response = requests.get(url, params=params)
         data = response.json()
         return data
@@ -53,11 +64,22 @@ class MainWindow(QDialog):
         return x_list, y_list
     
     # функция построения графика
-    def plot(self): 
+    def plot(self):
+        # очистка экземпляра фигуры перед построением графика
+        self.figure.clear()
         # создание координатной плоскости
         ax = self.figure.add_subplot(111)
+        # заголовок графика
+        plt.title('График рыночной стоимости Bitcoin', fontsize=14, fontweight='bold', color='blue')
+        # наименование осей координат
+        plt.xlabel('Период, год', fontsize=12, color='black')
+        plt.ylabel('Рыночная стоимость, $', fontsize=12, color='black')
+        # сетка графика
+        ax.grid()
         # построение линейного графика
-        ax.plot(*self.data_plot())
+        ax.plot(*self.data_plot(), label="Рыночная цена")
+        # легенда графика
+        ax.legend()
         # обновляем canvas
         self.canvas.draw()
 
@@ -65,5 +87,6 @@ class MainWindow(QDialog):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = MainWindow()
+    main.resize(1280, 720)
     main.show()
     sys.exit(app.exec_())
